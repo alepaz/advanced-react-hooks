52f9b5cede7a51f0939b4d32e069b7d938fd002e
Alejandro Paz <alepazte@hotmail.com>
2021-03-01 22:59:51 -0600

    solution from the course, reducer is outside the component

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,12 @@
 
 import React, { useReducer } from 'react'
 
+function countReducer(count, step) {
+  return count + step
+}
+
 function Counter({initialCount = 0, step = 1}) {
-
-  function countReducer(prevState, step) {
-    return prevState + step
-  }
-
   const [count, changeCount] = useReducer(countReducer, initialCount)
-
   const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
