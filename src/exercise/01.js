9a71b47b56314e105e029123645ed1d9fef7c890
Alejandro Paz <alepazte@hotmail.com>
2021-03-01 23:07:48 -0600

    extra credit 3, simulate setState from class components

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,15 +1,17 @@
 // useReducer: simple Counter
 // http://localhost:3000/isolated/exercise/01.js
 
-import React, { useReducer } from 'react'
+import React, {useReducer} from 'react'
 
-function countReducer(count, step) {
-  return count + step
+function countReducer(count, callback) {
+  return callback(count)
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  const [state, setState] = useReducer(countReducer, {count: initialCount})
+  const {count} = state
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
