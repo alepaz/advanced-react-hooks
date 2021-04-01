05c1ab5fc2c48773f6a0272ba7dfab277ae88e9f
Alejandro Paz <alepazte@hotmail.com>
2021-04-01 14:20:45 -0600

    feat: control inversion for useReducer

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,15 +3,18 @@
 
 import React, {useReducer} from 'react'
 
-function countReducer(state, newState) {
-  return {...state, ...newState}
+function countReducer(state, action) {
+  return action(state)
 }
 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = useReducer(countReducer, {count: initialCount})
   const {count} = state
-  const increment = () =>
-    setState({count: count + step})
+  const increment = () => {
+    return setState(currentState => {
+      return {count: currentState.count + step}
+    })
+  }
   return <button onClick={increment}>{count}</button>
 }
 
